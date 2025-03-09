5b8b06a9031e5e1e08c7cf842e43f62499427505
Zephyr Raine <zephyr.raine@gmail.com>
2025-03-09 10:35:57 +0100

    sfx

--- game/scenes/UsableObject.gd
+++ game/scenes/UsableObject.gd
@@ -1,9 +1,12 @@
 extends AnimatedSprite2D
 
 func use():
-	play("use")
+	if(!is_playing()):
+		play("use")
+		$Filet.play()
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if(is_playing() and area.get_parent().is_in_group("firefly")):
+		$Luciole.play()
 		area.get_parent().queue_free()
 		print("FIREFLY")
